5c98b6fa111ab040b082304ffd4a81d76de3ecb6
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-17 09:37:42 -0500

    added useRef hook in Login component to focus an input on load

--- p1-demo-frontend/src/Components/LoginRegister/Login.tsx
+++ p1-demo-frontend/src/Components/LoginRegister/Login.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Button, Container, Form } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 
@@ -7,11 +8,21 @@ export const Login:React.FC = () => {
         //(no more manual URL changing)
     const navigate = useNavigate()
 
+    //Using the useRef and useEffect hooks to focus our username input box on component load
+    const usernameRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        //if the current value of the ref is truthy...
+        if (usernameRef.current) {
+            usernameRef.current.focus(); //focus it so the user can type right away
+        }
+    }, []); //remember [] means this happens on component load
+
     return(
         /*Bootstrap gives us this Container element that does some default padding and centering*/
         <Container> 
 
-            <h1>Welcome</h1>
+            <h1 className="mb-5">Welcome</h1>
                 <h3>Please Log In:</h3>
                 
                 <div>
@@ -19,6 +30,8 @@ export const Login:React.FC = () => {
                         type="text"
                         placeholder="username"
                         name="username"
+                        ref={usernameRef} //attach our usernameRef here!
+                        //This is how our useRef knows what to focus.
                     />
                 </div>
 
@@ -31,8 +44,8 @@ export const Login:React.FC = () => {
                 </div>
                 
 
-            <Button className="btn-success m-1">Login</Button>
-            <Button className="btn-dark" onClick={()=>navigate("/register")}>Register</Button>
+            <Button variant="outline-success m-1">Login</Button>
+            <Button variant="outline-dark" onClick={()=>navigate("/register")}>Register</Button>
         </Container>
     )
 
