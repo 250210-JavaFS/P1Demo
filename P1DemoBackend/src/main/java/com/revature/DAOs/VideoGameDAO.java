c0c6f99c3cd8c54be71a0dc58aed5702d1056299
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-18 10:31:24 -0500

    setting up for insert game

---
+++ P1DemoBackend/src/main/java/com/revature/DAOs/VideoGameDAO.java
@@ -0,0 +1,9 @@
+package com.revature.DAOs;
+
+import com.revature.models.VideoGame;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //Make this Interface a bean
+public interface VideoGameDAO extends JpaRepository<VideoGame, Integer > {
+}
